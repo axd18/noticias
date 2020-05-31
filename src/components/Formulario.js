3f9f21af696931046c024f262f21e549fb352f4f
axd18 <adrianfernandezj@hotmail.com>
2020-05-30 21:33:23 -0300

    146-Colocando la categoria en el state

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Formulario.module.css';
 import useSelect from '../hooks/useSelect';
 
-const Formulario = () => {
+const Formulario = ({guardarCategoria}) => {
     
     const OPCIONES = [
         {value: 'general', label: 'General'},
@@ -17,11 +17,21 @@ const Formulario = () => {
 
     // Utilizar custom hook
     const [categoria, SelectNoticias] = useSelect('general', OPCIONES);
+
+    // submit al form, pasar de categoria al app.js
+    const buscarNoticas = e => {
+        e.preventDefault();
+
+        guardarCategoria(categoria);
+    }
+
     
     return ( 
         <div className={`${styles.buscador} row`}>
             <div className="col s12 m8 offset-m2">
-                <form>
+                <form
+                    onSubmit={buscarNoticas}
+                >
                     <h2 className={styles.heading}>Encuentra Noticas por Categoría</h2>
 
                     <SelectNoticias />
