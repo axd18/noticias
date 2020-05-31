0699f6b0dd8b645dee4d6f5375fc1907dfe7f4a2
axd18 <adrianfernandezj@hotmail.com>
2020-05-30 21:59:11 -0300

    148-Usando listado de noticias

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
@@ -6,6 +6,18 @@ function App() {
   
   // Definir la categoria y noticias
   const [categoria, guardarCategoria] = useState('');
+  const [ noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () =>{
+      const url = `http://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=36a05db9f9aa414eb74a59d2b8e57fc8`;
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      console.log(noticias);
+    }
+    consultarAPI();
+  }, [categoria]);
 
   return (
     <Fragment>
